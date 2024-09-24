ffe3fb9f2c91c02f565d516ac0a861530958e904
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-24 23:26:09 +0300

    Aaa

--- main.py
+++ main.py
@@ -1,75 +1,20 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.params import Depends
-from sqlalchemy.orm import Session
+from typing import Annotated
 
-import crud
-import schemas
-from db.engine import SessionLocal
-from db.models import PackagingType
+from fastapi import FastAPI, Depends
 
 app = FastAPI()
 
 
-def get_db() -> Session:
-    db = SessionLocal()
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
 
-    try:
-        yield db
-    finally:
-        db.close()
+CommonsDep = Annotated[dict, Depends(common_parameters)]
 
-
-@app.get("/")
-def root() -> dict:
-    return {"message": "Hello World"}
+@app.get("/items/")
+async def read_items(commons: CommonsDep):
+    return commons
 
 
-@app.get("/cheese_types/", response_model=list[schemas.CheeseType])
-def read_cheese_types(db: Session = Depends(get_db)):
-    return crud.get_all_cheese_types(db=db)
-
-
-@app.post("/cheese_types/", response_model=schemas.CheeseType)
-def create_cheese_type(
-        cheese_type: schemas.CheeseTypeCreate,
-        db: Session = Depends(get_db)
-):
-    db_cheese_type = crud.get_cheese_type_by_name(db=db, name=cheese_type.name)
-    if db_cheese_type:
-        raise HTTPException(
-            status_code=400,
-            detail="This name already exists"
-        )
-    return crud.create_cheese_type(db=db, cheese_type=cheese_type)
-
-
-@app.get("/cheese/", response_model=list[schemas.Cheese])
-def read_cheese(
-        packaging_type: PackagingType | None = None,
-        cheese_type: str | None = None,
-        db: Session = Depends(get_db)
-):
-    return crud.get_cheese_list(
-        db=db, packaging_type=packaging_type, cheese_type=cheese_type
-    )
-
-
-@app.get("/cheese/{cheese_id}/", response_model=schemas.Cheese)
-def read_single_cheese(
-        cheese_id: int,
-        db: Session = Depends(get_db)
-):
-    db_cheese = crud.get_cheese(db=db, cheese_id=cheese_id)
-
-    if db_cheese is None:
-        raise HTTPException(status_code=404, detail="Cheese not found")
-
-    return db_cheese
-
-
-@app.post("/cheese/", response_model=schemas.Cheese)
-def create_cheese(
-        cheese: schemas.CheeseCreate,
-        db: Session = Depends(get_db)
-):
-    return crud.create_cheese(db=db, cheese=cheese)
+@app.get("/users/")
+async def read_users(commons: CommonsDep):
+    return commons
